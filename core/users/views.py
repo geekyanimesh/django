cb752f34aeab40a10730315defe866853b85a2c4
Animesh Kumar <animesh.kumar9651@gmail.com>
2026-04-17 19:32:35 +0530

    basic django

--- core/users/views.py
+++ core/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the home page")
+    return render(request, "index.html")
 
 
 def success(request):
